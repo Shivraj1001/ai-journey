f0ef7609c471fab6c39059b200e031b7b7dda9e1
Shivraj Sondkar <shivrajsondkar101@gmail.com>
2025-12-18 23:04:24 +0530

    Add Python basics (input, output, variables)

---
+++ day2_python_basics.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city name: ")
+
+print("Hello", name)
+print("Next year you will be", age + 1)
+print("You are from", city)
